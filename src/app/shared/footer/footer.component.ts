dba0b22e7254d76ad634bed624e830f9cf5bb2ff
charlieTai <charlie.tai@tpisoftware.com>
2024-11-24 23:34:41 +0800

    add : 嘗試建立footer 共用組件，並引入app.components.html

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'guxn-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  thisYear!:Date;
 
+  constructor() {
+    this.thisYear = new Date();
+  }
 }
