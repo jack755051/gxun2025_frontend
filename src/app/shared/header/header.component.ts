870f6ac9b70a198335b09c15822fa9056f9b1f38
charlieTai <charlie.tai@tpisoftware.com>
2024-12-22 02:02:04 +0800

    add : 添加語系設定

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,18 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'gxun-header',
   standalone: true,
-  imports: [CommonModule,MatButtonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    TranslatePipe,
+    TranslateDirective
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
   @Input() headerButtonList:string[]=[];
 
-  constructor(){}
+  constructor() {
+    this.headerButtonList = [
+      'HEADER_PRODUCT',
+      'HEADER_SUPPORT',
+      'HEADER_ABOUT_US',
+      'HEADER_CONTACT_US',
+      'HEADER_FAQ'
+    ]
+  }
 
   ngOnInit(): void {
   }
