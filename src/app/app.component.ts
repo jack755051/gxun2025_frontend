91d26ad3e7e08be6afbee734d386f31b9ade87d6
jack755051 <jack755051@gmail.com>
2025-01-15 02:13:13 +0800

    add : 調整footer並設定簡易RWD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,31 +2,35 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SHARED_COMPONENTS } from './shared/shared';
 import { COMMON_COMPONENTS } from './common/common';
-import {
-    TranslateService,
-    TranslatePipe,
-    TranslateDirective
-} from "@ngx-translate/core";
 
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,...COMMON_COMPONENTS,TranslatePipe, TranslateDirective],
+  imports: [
+    RouterOutlet,
+    ...COMMON_COMPONENTS,
+    TranslatePipe,
+    TranslateDirective,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'gxun_frontend';
 
   constructor(private translate: TranslateService) {
-        this.translate.addLangs(['zh', 'en']);
-        this.translate.setDefaultLang('en');
-            // 在伺服器環境中使用默認語言
-        const defaultLang = 'en';
-        translate.use(defaultLang);
+    this.translate.addLangs(['zh', 'en']);
+    this.translate.setDefaultLang('en');
+    // 在伺服器環境中使用默認語言
+    const defaultLang = 'en';
+    translate.use(defaultLang);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'gxun-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-  thisYear!:Date;
+  thisYear!: Date;
 
   constructor() {
     this.thisYear = new Date();
