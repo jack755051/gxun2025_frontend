c5a7e2fb60e29aeedd77984a023ebaf2937535c5
jack755051 <jack755051@gmail.com>
2025-01-16 02:19:15 +0800

    add:添加基礎路由，調整布局

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gxun-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
