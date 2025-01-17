6453befed257c453eee5d22faca4b49e3659676d
jack755051 <jack755051@gmail.com>
2025-01-18 05:09:09 +0800

    add : 建立漂浮按鈕於component home ，並於app.config.ts 引入ant-design 按鈕

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { NzFloatButtonModule } from 'ng-zorro-antd/float-button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
-    selector: 'gxun-home',
-    imports: [],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'gxun-home',
+  imports: [NzFloatButtonModule, NzIconModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  openChange(status: boolean): void {
+    console.log(status);
+  }
 }
