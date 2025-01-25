1a5a84deb807da9fd7e096946704f065122fac7a
jack755051 <jack755051@gmail.com>
2025-01-25 14:15:32 +0800

    add : 建立卡片透過ant-design

--- src/app/common/card/card-item/card-item.component.ts
+++ src/app/common/card/card-item/card-item.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { ViewCard } from '../../../models/interface/card.interface';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'gxun-card-item',
-  imports: [],
+  imports: [CommonModule, NzCardModule, NzIconModule],
   templateUrl: './card-item.component.html',
   styleUrl: './card-item.component.scss',
 })
